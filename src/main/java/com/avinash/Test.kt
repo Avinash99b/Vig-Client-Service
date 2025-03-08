a8d7800ddd531b555cc5f13b7f9a08b9eef3f5de
Avinash <bathulaavi@gmail.com>
2025-03-08 15:22:15 +0530

    Updates

--- src/main/java/com/avinash/Test.kt
+++ src/main/java/com/avinash/Test.kt
@@ -7,13 +7,13 @@ class Test {
            println(getDiskSize())
        }
 
-       private fun getDiskSize(): Long {
+       private fun getDiskSize(): Int {
            return try {
                val result = executePowerShellCommand(
                    """(Get-WmiObject Win32_DiskDrive | Select-Object -ExpandProperty Size | Measure-Object -Sum).Sum""")
 
                val sizeInBytes = result.trim().toLongOrNull() ?: return 0
-               return sizeInBytes / (1024 * 1024 * 1024)
+               return (sizeInBytes / (1024 * 1024 * 1024)).toInt()
            } catch (e: Exception) {
                e.printStackTrace()
                0
